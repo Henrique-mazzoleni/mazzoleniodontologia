a66a8ecd789d1e7451d6e82e71d61c1829a70a5e
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-08-09 21:08:45 +0200

    hover links implemented

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,27 @@
-import styles from '../styles/Navbar.module.css';
-import Link from 'next/link';
+import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
 
-export default function Navbar () {
-    return <nav className={styles.navbar}>
-        <div className={styles.logo}>Mazzoleni Odontologia</div>
-        <div className={styles.links}>
-            <Link href='/'>Home</Link>
-            <Link href='/'>Sobre</Link>
-            <Link href='/'>Tratementos</Link>
-            <Link href='/'>Noticias</Link>
-            <Link href='/'>Contatos</Link>
-        </div>
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>Mazzoleni Odontologia</div>
+      <ul className={styles.links}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/">Sobre</Link>
+        </li>
+        <li>
+          <Link href="/">Tratementos</Link>
+        </li>
+        <li>
+          <Link href="/">Noticias</Link>
+        </li>
+        <li>
+          <Link href="/">Contatos</Link>
+        </li>
+      </ul>
     </nav>
+  );
 }
