95b9590f6c51a425ca5083b7cb495abc689d745c
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-08-21 12:48:46 +0200

    footer added

--- components/Footer.js
+++ components/Footer.js
@@ -11,39 +11,43 @@ import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.logo}>
-        <Image
-          src="/Mazzoleni_logo.jpg"
-          alt="Mazzoleni Logo"
-          placeholder="empty"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-      <div className={styles.links}>
-        <a
-          href="https://www.facebook.com/Mazzoleni-Odontologia-100229098203417/?ref=page_internal"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faFacebook} size="2x" />
-        </a>
-        <a
-          href="https://www.instagram.com/mazzoleniodontologia/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
-        </a>
-        <a
-          href="https://wa.me/5511950002799"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-        </a>
-      </div>
-    </footer>
+    <div className={styles.wrapper}>
+      <footer className={styles.footer}>
+        <div className={styles.logo}>
+          <Image
+            src="/images/Mazzoleni_logo.jpg"
+            alt="Mazzoleni Logo"
+            placeholder="empty"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="top center"
+          />
+        </div>
+        <div className={styles.pages}></div>
+        <div className={styles.links}>
+          <a
+            href="https://www.facebook.com/Mazzoleni-Odontologia-100229098203417/?ref=page_internal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+          <a
+            href="https://www.instagram.com/mazzoleniodontologia/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+          <a
+            href="https://wa.me/5511950002799"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 }
