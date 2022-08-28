3d7841575835487c1e9d877f788eb101c95150b0
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-08-28 14:01:44 +0200

    navbar on scroll implemented

--- components/HeadComponent.js
+++ components/HeadComponent.js
@@ -4,11 +4,14 @@ export default function HeadComponent(props) {
   return (
     <Head>
       <title>Mazzoleni Odontologia | {props.title}</title>
-      <meta
-        name="description"
-        content={props.content}
-      />
+      <meta name="description" content={props.content} />
       <link rel="icon" href="/images/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
     </Head>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,53 @@
-import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
+import Image from "next/image";
+
+import styles from "../styles/Navbar.module.css";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const image = (
+    <div className={styles.logo}>
+      <Image
+        src="/images/Mazzoleni_logo.jpg"
+        alt="Mazzoleni Logo"
+        placeholder="empty"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="top center"
+      />
+    </div>
+  );
+
+  const text = (
+    <div className={styles["logo-text"]}>
+      <h3>Mazzoleni Odontologia</h3>
+    </div>
+  );
+
+  const [logo, setLogo] = useState(image);
+  const [navClass, setNavClass] = useState(styles['navbar-top'])
+
+  const scrollHandler = () => {
+    if (window.pageYOffset === 0) {
+      setLogo(image);
+      setNavClass(styles['navbar-top'])
+    } else {
+      setLogo(text);
+      setNavClass(styles['navbar-scroll'])
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={navClass}>
       <div className={styles.content}>
-        <div className={styles.logo}>Mazzoleni Odontologia</div>
+        {logo}
         <ul className={styles.links}>
           <li>
             <Link href="/">Home</Link>
